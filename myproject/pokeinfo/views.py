e0b0b9f7552741744230667872a61e0bb39c49ac
MattFrankowski <mateofrankowski@gmail.com>
2020-09-21 16:40:35 +0200

    integrate PokeAPI

--- myproject/pokeinfo/views.py
+++ myproject/pokeinfo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from .services import get_pokemon
 
 class IndexView(TemplateView):
     template_name = "pokeinfo/index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # add pokemon info to context
+        context["pokemon"] = get_pokemon("charizard")
+        return context
